b409b661e85d7753a40171bbc2f0999c5f2befed
Jesse Omoefe <jesseomoefe@Jesses-MacBook-Pro.local>
2019-03-21 18:55:49 +0100

    ft(server): create post team endpoint
    [Finishes #164802340]

--- source/controller.js
+++ source/controller.js
@@ -2,19 +2,12 @@ import Pool from '../db/connection';
 
 class Team {
   static async createTeam(req, res) {
-    const { teamName } = req.body;
-    const { rowCount } = await Pool.query('INSERT INTO team ("name") VALUES ($1) RETURNING *', [teamName]);
-    if (rowCount > 0) {
-      res.status(201).json({
-        status: 201,
-        message: 'Team created',
-      });
-    } else {
-      res.status(409).json({
-        status: 409,
-        error: 'Team already exists',
-      });
-    }
+    const { teamName, members } = req.body;
+    const result = await Pool.query('SELECT * FROM add_team($1, $2::jsonb)', [teamName, JSON.stringify(members)]);
+    return res.status(201).json({
+      status: 201,
+      data: result,
+    });
   }
 }
 
--- source/routes.js
+++ source/routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import Team from './controller';
+import { checkCreateTeam } from './validations';
 
 const Router = express.Router();
 
-Router.post('/team', Team.createOffice);
+Router.post('/team', checkCreateTeam, Team.createTeam);
 
 export default Router;
