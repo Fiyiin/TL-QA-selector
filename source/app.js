919d5b3ec93f8e2f1da942ffa47f83910b8fd83e
Fiyin Kolawole <kolawolemmanuel@hotmail.com>
2019-03-21 17:47:03 +0100

    Create db connection

--- source/app.js
+++ source/app.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import routes from './routes';
+import bodyparser from 'body-parser';
+import '@babel/polyfill';
 
+import routes from './routes';
 
 const PORT = process.env.PORT || 7000;
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use('/api/v1', routes);
 app.use((req, res, next) => {
@@ -27,7 +28,7 @@ app.use((err, req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Sever running');
+  console.log(`Sever running on port ${PORT}`);
 });
 
 
--- source/controller.js
+++ source/controller.js
@@ -1,8 +1,20 @@
-import pool from '../models';
+import Pool from '../db/connection';
 
 class Team {
   static async createTeam(req, res) {
-    
+    const { teamName } = req.body;
+    const { rowCount } = await Pool.query('INSERT INTO team ("name") VALUES ($1) RETURNING *', [teamName]);
+    if (rowCount > 0) {
+      res.status(201).json({
+        status: 201,
+        message: 'Team created',
+      });
+    } else {
+      res.status(409).json({
+        status: 409,
+        error: 'Team already exists',
+      });
+    }
   }
 }
 
