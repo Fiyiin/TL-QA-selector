7da36bee908946faa808fca4a07408b4584733c6
Kayode Okunlade <kayodeokunlade@Kayodes-MacBook-Pro.local>
2019-03-22 17:23:27 +0100

    Add integration test

--- source/test/selectorTest.js
+++ source/test/selectorTest.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill';
 import { describe, it } from 'mocha';
 import { use, request, expect } from 'chai';
 import chaiHttp from 'chai-http';
@@ -9,10 +10,32 @@ describe('Test TL-QA-Selector endpoints', () => {
     const res = await request(app)
       .get('/notfound');
     expect(res).to.have.status(404);
-    expect(res.body).to.have.property('erorr');
     expect(res.body.error).to.have.property('status')
       .to.be.equal(404);
     expect(res.body.error).to.have.property('message')
       .to.be.equal('Not found');
+  });
+
+  it('Should register a team and the team members', async () => {
+    const data = {
+      teamName: 'ivy',
+      members: ['Joyce', 'Kayode', 'Jude', 'Fiyin', 'Kossy', 'Jesse', 'Innocent'],
+    };
+    const res = await request(app)
+      .post('/api/v1/team')
+      .send(data);
+    expect(res).to.have.status(201);
+    expect(res.body).to.have.property('status')
+      .to.be.equal(201);
+    expect(res.body).to.have.property('data');
+  });
+
+  it('should get the team lead and 2 QAs', async () => {
+    const res = await request(app)
+      .get('/api/v1/generate/ivy');
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property('status')
+      .to.be.equal(200);
+    expect(res.body).to.have.property('data');
   });
 });
