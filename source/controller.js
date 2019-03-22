92d417d3891176b896243b69137cf71add23f6ee
Jesse Omoefe <jesseomoefe@Jesses-MacBook-Pro.local>
2019-03-22 15:08:24 +0100

    feat(server): return discriptive error message

--- source/controller.js
+++ source/controller.js
@@ -3,11 +3,20 @@ import Pool from '../db/connection';
 class Team {
   static async createTeam(req, res) {
     const { teamName, members } = req.body;
-    const result = await Pool.query('SELECT * FROM add_team($1, $2::jsonb)', [teamName, JSON.stringify(members)]);
-    return res.status(201).json({
-      status: 201,
-      data: result,
-    });
+    try {
+      const result = await Pool.query('SELECT * FROM add_team($1, $2::jsonb)', [teamName, JSON.stringify(members)]);
+      return res.status(201).json({
+        status: 201,
+        data: result,
+      });
+    } catch(err) {
+      if (err.code == 23505) {
+        return res.status(409).json({
+          status: 409,
+          error: 'this team name already exist',
+        });
+      } 
+    }
   }
 }
 
